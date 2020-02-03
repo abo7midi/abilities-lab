0324f33d6759fc2917ae31ae306f9d5f74ba2282
khuloodah <54434325+khuloodah@users.noreply.github.com>
2020-02-03 10:24:35 +0300

    test2

--- app/controller/adminController.php
+++ app/controller/adminController.php
@@ -6,27 +6,15 @@
 class adminController extends Controller
 {
 
-  public function index()
-  {
-$this->model('News');
-    $this->view('admin'.DIRECTORY_SEPARATOR.'index',['news'=>$this->model->all()]);
+    public function index()
+    {
+        $this->model('News');
+        $this->view('admin' . DIRECTORY_SEPARATOR . 'index', ['news' => $this->model->all()]);
 
-    $this->view->pageTitle='admin index';
-    $this->view->render();
+        $this->view->pageTitle = 'admin index';
+        $this->view->render();
 
-  }
-
-  public function index2()
-  {
-$this->model('News');
-    $this->view('admin'.DIRECTORY_SEPARATOR.'index',['news'=>$this->model->all()]);
-
-    $this->view->pageTitle='admin index';
-    $this->view->render();
-
-  }
-
-
+    }
 
 
 
@@ -34,23 +22,4 @@ $this->model('News');
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- ?>
+?>
