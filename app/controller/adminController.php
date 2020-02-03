c0248e01f76f3ba8ed5da4da8522fe0ebf573731
khuloodah <54434325+khuloodah@users.noreply.github.com>
2020-02-03 09:54:08 +0300

    test

--- app/controller/adminController.php
+++ app/controller/adminController.php
@@ -16,6 +16,16 @@ $this->model('News');
 
   }
 
+  public function index2()
+  {
+$this->model('News');
+    $this->view('admin'.DIRECTORY_SEPARATOR.'index',['news'=>$this->model->all()]);
+
+    $this->view->pageTitle='admin index';
+    $this->view->render();
+
+  }
+
 
 
 
