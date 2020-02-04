8f373ce2c1e1064d6588a0606fbbbed1059571f7
khuloodah <54434325+khuloodah@users.noreply.github.com>
2020-02-04 16:15:42 +0300

    viewprofile&modify

---
+++ app/controller/profileController.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alzubeer
+ * Date: 03/02/2020
+ * Time: 09:40 م
+ */
+
+class profileController extends Controller
+{
+    public function index()
+    {
+//        if (!empty($user)) {
+//            Session::loggIn([$user[0]['user_id']]);
+        $this->model('Profile');
+        $this->view('admin'.DIRECTORY_SEPARATOR.'showProfile',['Profile'=>$this->model->all()]);
+        $this->view->pageTitle='admin showProfile';
+        $this->view->render();
+
+    }
+//}
+
+//    edit profile
+////controller for adding user by admin
+///
+    public function editProfile($id)
+    {
+
+        $x= $this->model('Profile');
+        // check if there submit
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            $validate=Validation::required(['','full_name','email','phone']);
+
+            if ($validate['status'] == 1)
+            if ($_FILES)
+            {
+                $upload = 1;
+                $file = $_FILES["fileToUpload"]["name"];
+                $file_tmp = $_FILES["fileToUpload"]["tmp_name"];
+                $file_size = $_FILES["fileToUpload"]["size"];
+                $target_dir = ROOT."public".DIRECTORY_SEPARATOR."pictures".DIRECTORY_SEPARATOR;
+                $updatep = Helper::uploadFile($target_dir, $file, $file_tmp, $file_size);
+            }
+
+
+            $modify= array(
+                ':fullname' => htmlentities($_REQUEST['full_name']),
+                ':password' =>  Hashing::init($_REQUEST['password']),
+                ':email' => htmlentities($_REQUEST['email']),
+                ':mobile' => htmlentities($_REQUEST['phone']),
+                ':picture' => $updatep,
+                ':id'=>$id
+
+            );
+
+
+
+
+            if ($x->updateProfile($modify)) {
+                Message::setMessage(1,'main',' تم تحديث الفئة بنجاح');
+            }
+        }
+
+
+
+
+//        $modifying=$this->model("Profile");//isset()?$this->model: $this->model('Profile');
+
+        # show form view  to add new user
+
+
+        $this->view('admin'.DIRECTORY_SEPARATOR.'modifyProfile',['profile'=>$x->find( array(0 =>$id))]);
+
+        $this->view->pageTitle='edit Profile';
+        $this->view->render();
+    }
+
+}
---
+++ app/model/Profile.php
@@ -0,0 +1,40 @@
+<?php
+
+
+class Profile
+{
+    protected  $data_file;
+    protected $db;
+    protected $inventory=[ ];
+
+    function __construct()
+    {
+        $this->db=new Model();
+    }
+// return all row of table of users
+    public function all()
+    {
+//
+//           $id=$_session=['user_id'];
+        return $this->db->query("select  users.user_id,full_name as fullname,phone as mobile,user_email as email ,image as pacture,exams.exam_pass_mark as degree 
+                     from exams INNER JOIN users on exams.user_id= users.user_id ");
+    }
+
+
+    public function updateProfile($aData)
+    {
+
+        $oStmt = $this->db->preparation('UPDATE `users` SET `full_name`=:fullname,`user_pwd`=:password,`user_email`=:email,`phone`=:mobile,image=:picture WHERE user_id=:id
+');
+      return $oStmt->execute($aData);
+
+    }
+    public function find($aData)
+    {
+        $oStmt = $this->db->preparation('SELECT * FROM users WHERE user_id =?');
+        $oStmt->execute($aData);
+        return $oStmt->fetch();
+
+    }
+
+}
